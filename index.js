072e697836c7e6b3f46f54c40cf2ed72f5ce57ba
Capicapilla <javier.capicapilla@gmail.com>
2025-04-13 12:40:29 +0200

    Solved lab

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+let names = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5;
 
 // Print out the concatenated string
 
+console.log(names)
 
 
 
@@ -23,10 +25,18 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+let part1UpperCase = part1.slice(0, -1) + part1.slice(-1).toUpperCase()
+console.log(part1UpperCase)
+
+let part2UpperCase = part2.slice(0, -1) + part2.slice(-1).toUpperCase()
+console.log(part2UpperCase)
+
+let phrasesMix = part1UpperCase + part2UpperCase
+
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(phrasesMix)
 
 
 /*******************************************
@@ -36,11 +46,12 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+let percent = 84 * 0.15;
 
 // Print out the tipAmount
 
 
-
+console.log(percent)
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
@@ -48,10 +59,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+let randomNum = Math.floor((Math.random()* 10 )+ 1);
 
 // Print the generated random number
 
-
+console.log(randomNum)
 
 /*******************************************
     Iteration 3.1 | Booleans
